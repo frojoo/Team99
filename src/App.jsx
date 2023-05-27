64b544bde869532bee9318f403112182ed991939
frojoo <wndyd29605@gmail.com>
2023-05-27 14:04:20 +0900

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen">
       <Header />
-      Hello!
+      Hello!!
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="w-full flex justify-center items-center h-[90px] text-white text-[16px] bg-black bg-opacity-70">
+      <div className="max-w-screen-xl mx-auto flex grow justify-between items-center">
+        <button className="text-3xl font-bold">Team 99</button>
+        <div className="flex grow gap-32 ml-32 items-center font-bold text-md">
+          <button>프로젝트 소개</button>
+          <button>주요사업</button>
+          <button>NFT</button>
+          <button>DeFi</button>
+        </div>
+        <button>Log-In</button>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
